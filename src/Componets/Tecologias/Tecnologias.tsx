c2b71ac7938a7ee5943172e10bb6ec731b93ec54
Fere <tiran.sc.f@gmail.com>
2026-04-16 04:04:37 -0600

    se agrago paddin a los elementos de tecnologias

--- src/Componets/Tecologias/Tecnologias.tsx
+++ src/Componets/Tecologias/Tecnologias.tsx
@@ -12,65 +12,65 @@ export const Tecnologias = () => {
 				<ul className="flex flex-row gap-10 justify-center items-center flex-wrap">
 
 					<li>
-						<figure className="flex flex-col">
-							<Html className="size-15  dark:drop-shadow-[0_4px_20px_#e34f268f] " />
-							<figcaption>HTML</figcaption>
+						<figure className="flex flex-col ">
+							<Html className="size-15 dark:drop-shadow-[0_4px_20px_#e34f268f] " />
+							<figcaption className ="pt-3">HTML</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<Css className=" size-15 dark:drop-shadow-[0_4px_20px_#264de48f]" />
-							<figcaption>CSS</figcaption>
+							<figcaption className ="pt-3" >CSS</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<Javascrip className="size-15 dark:drop-shadow-[0_4px_20px_#efdb508f]" />
-							<figcaption>JavaScript</figcaption>
+							<figcaption className ="pt-3">JavaScript</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<React className="size-15 filter dark:drop-shadow-[0_4px_20px_#58c4dc8f]" />
-							<figcaption>React</figcaption>
+							<figcaption className ="pt-3">React</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<Bun className="size-15 filter dark:drop-shadow-[0_4px_20px_#fbf0df8f]" />
-							<figcaption>Bun</figcaption>
+							<figcaption className ="pt-3">Bun</figcaption>
 						</figure>
 					</li> 
 
 					<li>
 						<figure className="flex flex-col ">
 							<Tailwind className="size-15 dark:drop-shadow-[0_4px_20px_#38bdf88f]" />
-							<figcaption>Tailwind</figcaption>
+							<figcaption className ="pt-3">Tailwind</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<TypeScript className="size-15 dark:drop-shadow-[0_4px_20px_#3178c68f]" />
-							<figcaption>TypeScript</figcaption>
+							<figcaption className ="pt-3">TypeScript</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<GitHub className="size-15 dark:drop-shadow-[0_4px_20px_#ffffff8f]"  />
-							<figcaption>GitHub</figcaption>
+							<figcaption className ="pt-3">GitHub</figcaption>
 						</figure>
 					</li>
 
 					<li>
 						<figure className="flex flex-col ">
 							<Npm className="size-15 dark:drop-shadow-[0_4px_20px_#cc000075]" />
-							<figcaption>NPM</figcaption>
+							<figcaption className ="pt-3">NPM</figcaption>
 						</figure>
 					</li>
 
